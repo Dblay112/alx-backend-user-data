8fdee490243c6f0e4978e1add4afa2cbb91f417f
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-04-16 21:46:41 +0000

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -44,4 +44,20 @@ class BasicAuth(Auth):
         if not user.is_valid_password(user_pwd):
             return None
 
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        current user.
+        """
+        authorization_header = self.authorization_header(request)
+
+        base64_authorization_header = self.extract_base64_authorization_header(
+            authorization_header)
+
+        decoded_base64_authorization_header = self.decode_base64_authorization_header(
+            base64_authorization_header)
+
+        user_email, user_pwd = self.extract_user_credentials(
+            decoded_base64_authorization_header)
+
+        return self.user_object_from_credentials(user_email, user_pwd)
         return user
