c48dfbac1c6664fa7ed84f1ab11cb0512ba11d0a
Bright Selorm Djogatse <brightdjogatse@gmail.com>
2024-04-16 16:40:40 +0000

    pushing appropriate files

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
-"""module"""
-from flask import Blueprint, abort
+""" Module of Index views
+"""
+from flask import jsonify, abort
+from api.v1.views import app_views
 
-blueprint = Blueprint('index', __name__)
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
+    return jsonify({"status": "OK"})
 
 
-@blueprint.route('/api/v1/unauthorized')
-def unauthorized_endpoint():
-    """unauthorized endpoint method"""
-    abort(401)
-    
-def register_views(app):
-    """register views method"""
-    app.register_blueprint(blueprint)
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
