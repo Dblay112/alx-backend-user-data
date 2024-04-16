72e5fa4d493d0f46e8b801fd4522fec89d5c6c3d
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-04-16 19:01:14 +0000

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,12 +2,12 @@
 """
 Route module for the API
 """
+
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS, cross_origin
 import os
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -17,11 +17,9 @@ auth_type = getenv('AUTH_TYPE')
 
 if auth_type == 'auth':
     from api.v1.auth.auth import Auth
-
     auth = Auth()
 elif auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
-
     auth = BasicAuth()
 
 
@@ -30,6 +28,7 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
@@ -57,7 +56,7 @@ def before_request() -> None:
 
     if not auth.current_user(request):
         abort(403)
-        
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
