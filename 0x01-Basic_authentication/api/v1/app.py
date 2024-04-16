0b3ad56e6edf11396e0e6dfa41b0037dc9232e4a
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-04-16 18:40:39 +0000

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,8 +12,13 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
 
+auth_type = getenv('AUTH_TYPE')
+if auth_type == 'auth':
+    from api.v1.auth.auth import Auth
+    
+    auth = Auth()
+    
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -31,6 +36,23 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """not allowed access handler"""
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request() -> None:
+    """before request handler"""
+    if not auth or not auth.require_auth(request.path,
+                                         ['/api/v1/status/',
+                                          '/api/v1/unauthorized/',
+                                          '/api/v1/forbidden/']):
+        return
+
+    if not auth.authorization_header(request):
+        abort(401)
+
+    if not auth.current_user(request):
+        abort(403)
+        
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
