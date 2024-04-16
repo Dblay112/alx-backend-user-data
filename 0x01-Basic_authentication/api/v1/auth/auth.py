6d74be0c946c2d95836b40edbdec4c97dac4f2c5
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-04-16 18:07:37 +0000

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,8 +9,17 @@ from typing import List, TypeVar
 class Auth:
     """Base authentication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """require auth method"""
-        return False
+        """Require auth method"""
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        path = path.rstrip("/")
+        excluded_paths = [p.rstrip("/") for p in excluded_paths]
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization header method"""
