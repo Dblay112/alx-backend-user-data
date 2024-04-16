eed172cfff859859ec2bca1d7ddfd8b1094a9b41
Bright Selorm Djogatse <brightdjogatse@gmail.com>
2024-04-16 18:57:06 +0000

    Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,7 +18,11 @@ if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     
     auth = Auth()
-    
+elif auth_type = 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+
+    auth = BasicAuth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
