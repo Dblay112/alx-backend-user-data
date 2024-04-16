acc0048d20a0e99893de46ca2ea981cae5363593
Bright Selorm Djogatse <brightdjogatse@gmail.com>
2024-04-16 17:53:27 +0000

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
-""" Module of Index views
 """
-from flask import jsonify, abort
-from api.v1.views import app_views
+Module for aunthentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Base authentication class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require auth method"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """authorization header method"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user methid"""
+        return None
