10d846d4f6edd49e6a890fa59f7b903d4e08b92d
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-04-16 19:28:28 +0000

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,3 +10,15 @@ import base64
 
 class BasicAuth(Auth):
     """Basic authentication class."""
+
+    def __init__(self) -> None:
+        """Initialization of BasicAuth class"""
+        super().__init__()
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extract_base64_authorization_header method"""
+        if not authorization_header or not isinstance(authorization_header, str) or authorization_header[:6] != 'Basic ':
+            return None
+        return " ".join(authorization_header.split(' ')[1:])
+        
+        
