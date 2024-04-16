4a18e788e1ba1ea23edc8c2f7e9ee1e5cbe201a9
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-04-16 17:18:51 +0000

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -13,11 +13,6 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized_endpoint() -> str:
-    """unauthorized endpoint method"""
-    abort(401)
-
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
@@ -29,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ unauthorized method
+    returns:
+    error object
+    """
+    abort(401)
