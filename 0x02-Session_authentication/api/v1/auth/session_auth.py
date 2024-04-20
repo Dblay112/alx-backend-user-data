18b783db078d8f2334992658c06a7c4cd9d5195e
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-04-20 13:45:28 +0000

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,6 +9,15 @@ from models.user import User
 
 class SessionAuth(Auth):
   """ session authentication class that inherits Auth"""
+  user_id_by_session_id = {}
   def __init__(self) -> None:
     """class initialization"""
     super().__init__()
+
+  def create_session(self, user_id: str = None) -> str:
+    """an instance method that creates a session for user id"""
+    if not user_id or not isinstanceof(user_id, str):
+      return None
+    session_id = str(uuid.uuid4())
+    self.user_id_by_session_id[session_id] = user_id
+    return session_id
