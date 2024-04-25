a82f283a886f151cf5b35c500a00af636401573e
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-04-25 11:50:20 +0000

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,7 +31,8 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database"""
-        user = User(email=email, hashed_password=hashed_password)
+        user = User(email=email, hashed_password=hashed_password,
+                    session_id="", reset_token="")
         self._session.add(user)
-        self._session.commit()
-        return user
+
+        return self._session.query(User).filter_by(email=email).first()
